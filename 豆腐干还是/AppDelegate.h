bf2224c458d13c7a0979ccabdca432d21d1f58fe
xia <728464934@qq.com>
2015-06-24 23:19:14 +0800

    Initial Commit

---
+++ 豆腐干还是/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  豆腐干还是
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 lanou3G.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 豆腐干还是/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  豆腐干还是
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 lanou3G.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
